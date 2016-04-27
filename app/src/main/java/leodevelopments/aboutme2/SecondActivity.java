9106a3b2ebcbcb163f3daedc7c230033069db70c
Флоров Леонид <l.florov@kupivip.local>
2016-04-27 19:38:26 +0300

    Неудачные попытки добавления диалогового окна.

--- app/src/main/java/leodevelopments/aboutme2/SecondActivity.java
+++ app/src/main/java/leodevelopments/aboutme2/SecondActivity.java
@@ -1,24 +1,65 @@
 package leodevelopments.aboutme2;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
 
 public class SecondActivity extends AppCompatActivity {
+
+    AlertDialog.Builder ad;
+    Context context;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.second_layout);
+
+        context = SecondActivity.this;
+        String title = "Выбор есть всегда";
+        String message = "Выбери пищу";
+        String button1String = "Вкусная пища";
+        String button2String = "Здоровая пища";
+
+        ad = new AlertDialog.Builder(context);
+        ad.setTitle(title);  // заголовок
+        ad.setMessage(message); // сообщение
+        ad.setPositiveButton(button1String, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int arg1) {
+                Toast.makeText(context, "Вы сделали правильный выбор",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+        ad.setNegativeButton(button2String, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int arg1) {
+                Toast.makeText(context, "Возможно вы правы", Toast.LENGTH_LONG)
+                        .show();
+            }
+        });
+        ad.setCancelable(true);
+        ad.setOnCancelListener(new DialogInterface.OnCancelListener() {
+            public void onCancel(DialogInterface dialog) {
+                Toast.makeText(context, "Вы ничего не выбрали",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
+    public void onClick(View v) {
+        ad.show();
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
@@ -38,9 +79,7 @@ public class SecondActivity extends AppCompatActivity {
             default:
                 return super.onOptionsItemSelected(item);
         }
-
     }
-
     public void onSettingsMenuClick(MenuItem item) {
         TextView infoTextView = (TextView) findViewById(R.id.secondViewTestText);
         infoTextView.setText("Вы нажали настройки, но лучше бы нажали что-то другое ;)");

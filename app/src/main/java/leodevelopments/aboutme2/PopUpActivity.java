9da066b6503f3d7a61b4238d5158b69e9a8ab267
leoflorov <leoflorov@mail.ru>
2016-05-02 00:08:48 +0300

    Тест ветвления (reverted from commit 9164485f7f18aa13ba8a8cfde3e541c6fddae716)

--- app/src/main/java/leodevelopments/aboutme2/PopUpActivity.java
+++ app/src/main/java/leodevelopments/aboutme2/PopUpActivity.java
@@ -3,7 +3,6 @@ package leodevelopments.aboutme2;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.view.menu.MenuView;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -82,9 +81,4 @@ public class PopUpActivity extends AppCompatActivity {
         });
         popupMenu.show();
     }
-    public  void fuckinShit(Menu MenuView.ItemView) {
-
-
-    }
 }
-

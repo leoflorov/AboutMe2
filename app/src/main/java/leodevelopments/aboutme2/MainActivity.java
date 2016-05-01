1496711690bad511085a5d82f8de36a65f5780d9
leoflorov <leoflorov@mail.ru>
2016-05-01 23:57:38 +0300

    Удалил ненужный класс PopUpActivity.java, а так же удалил все незадействованные в приложении библиотеки. Стало чисто и аккуратно.

--- app/src/main/java/leodevelopments/aboutme2/MainActivity.java
+++ app/src/main/java/leodevelopments/aboutme2/MainActivity.java
@@ -1,12 +1,9 @@
 package leodevelopments.aboutme2;
 
 import android.content.Intent;
-import android.media.Image;
-import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
--- app/src/main/java/leodevelopments/aboutme2/SecondActivity.java
+++ app/src/main/java/leodevelopments/aboutme2/SecondActivity.java
@@ -2,18 +2,14 @@ package leodevelopments.aboutme2;
 
 import android.content.Context;
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
-
 
 public class SecondActivity extends AppCompatActivity {
 

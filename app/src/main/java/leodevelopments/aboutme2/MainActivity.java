74c1e39d90f4f2f8374ba190227adce647e11d31
leoflorov <leoflorov@mail.ru>
2016-05-08 23:06:37 +0300

    Убрал ненужный класс, чтобы он не грузил приложение.

--- app/src/main/java/leodevelopments/aboutme2/MainActivity.java
+++ app/src/main/java/leodevelopments/aboutme2/MainActivity.java
@@ -4,9 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
-import android.widget.ListView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
